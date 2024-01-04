0f467f6182255c8c02e51ac26cdc25d4ae1e2e06
meilizheng <116130097+meilizheng@users.noreply.github.com>
2024-01-04 11:36:50 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GAGitHubBasics_MeiliZheng
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
