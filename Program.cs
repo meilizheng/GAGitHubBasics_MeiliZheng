9ee34bbcefd10ae9d381fc2deeb67bf3db58f9e4
meilizheng <116130097+meilizheng@users.noreply.github.com>
2024-01-04 11:39:26 -0800

    Update Program

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, Professor!");
+            Console.WriteLine("Meili Zheng, I want to learn AI");
         }
     }
 }
